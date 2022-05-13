b97ead4b3c381313e62290c589cccfacb52d2e87
leochen <Leo_Chen_zy@hotmail.com>
2022-05-13 17:26:54 -0500

    commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import numpy as np
 
 # reads the gme csv into a matrix
 with open('gme.csv') as file:
@@ -12,5 +13,30 @@ gme = list(reversed(gme))
 for loop in range(len(gme)):
     gme[loop][0] = loop
 
-A = list()
-for loop in range(len(gme)):
+# y = C + Dx
+# Ax = b
+# x = (AᵀA)⁻¹ * Aᵀ * b
+
+
+# non linear regression
+def regression(power: int):
+    matrix_a = list()
+    for row in range(len(gme)):
+        # add a column of ones
+        matrix_a.append([1])
+
+    for row in range(len(gme)):
+        for exponent in range(power):
+            matrix_a[row].append((gme[row][0]) ** (exponent + 1))
+
+    b = list()
+    for row in range(len(gme)):
+        b.append(float(gme[row][1]))
+
+    # x = (AᵀA)⁻¹ * Aᵀ * b
+    print(np.dot(np.dot(np.linalg.inv(np.dot(np.transpose(matrix_a), matrix_a)), np.transpose(matrix_a)), b))
+
+
+regression(1)
+regression(2)
+regression(3)
