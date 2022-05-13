be7109e435c7696d11a9f46d376e7e0bda0098a8
leochen <Leo_Chen_zy@hotmail.com>
2022-05-12 22:54:50 -0500

    commit

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ with open('gme.csv') as file:
     reader = csv.reader(file)
     gme = list(reader)
 
+del gme[0]
+gme = list(reversed(gme))
+
+# change the date of the csv, where the first number is the number of trading days since May 12th, 2021
+for loop in range(len(gme)):
+    gme[loop][0] = loop
+
+A = list()
+for loop in range(len(gme)):
